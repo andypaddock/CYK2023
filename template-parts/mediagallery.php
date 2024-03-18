0d06d9b346389edd574de0194b519cc2b70dacb4
Andy Paddock <appaddock@gmail.com>
2024-03-18 11:13:02 +0000

    gallery changes

--- template-parts/mediagallery.php
+++ template-parts/mediagallery.php
@@ -3,35 +3,35 @@
 
 
         <?php
-// Assuming get_sub_field('images'), get_sub_field('videos'), and get_sub_field('web_cams') return arrays
+        // Assuming get_sub_field('images'), get_sub_field('videos'), and get_sub_field('web_cams') return arrays
+        
+        $filters = array();
 
-$filters = array();
+        if (get_sub_field('images')) {
+            $filters[] = '<button class="filters" type="button" data-filter=".gallery-image">Photos</button>';
+        }
 
-if (get_sub_field('images')) {
-    $filters[] = '<button class="filters" type="button" data-filter=".gallery-image">Photos</button>';
-}
+        if (get_sub_field('videos')) {
+            $filters[] = '<button class="filters" type="button" data-filter=".gallery-video">Video</button>';
+        }
 
-if (get_sub_field('videos')) {
-    $filters[] = '<button class="filters" type="button" data-filter=".gallery-video">Video</button>';
-}
+        if (get_sub_field('web_cams')) {
+            $filters[] = '<button class="filters" type="button" data-filter=".gallery-web">Webcam</button>';
+        }
 
-if (get_sub_field('web_cams')) {
-    $filters[] = '<button class="filters" type="button" data-filter=".gallery-web">Webcam</button>';
-}
+        // Check if there is more than one filter
+        if (count($filters) > 1) {
+            echo '<div class="gallery-filters controls">';
+            echo '<button class="filter-all" type="button" data-filter="all">Filter:</button>';
 
-// Check if there is more than one filter
-if (count($filters) > 1) {
-    echo '<div class="gallery-filters controls">';
-    echo '<button class="filter-all" type="button" data-filter="all">Filter:</button>';
-    
-    // Output the filter buttons
-    foreach ($filters as $filter) {
-        echo $filter;
-    }
+            // Output the filter buttons
+            foreach ($filters as $filter) {
+                echo $filter;
+            }
 
-    echo '</div>';
-}
-?>
+            echo '</div>';
+        }
+        ?>
 
 
 
@@ -39,12 +39,12 @@ if (count($filters) > 1) {
         <div class="outer-container gallery-outer mb10">
             <div class="gallery-wrapper">
                 <?php $images = get_sub_field('images');
-$size = 'large'; // (thumbnail, medium, large, full or custom size)
-$original = 'full';
-if ($images) : ?>
+                $size = 'large'; // (thumbnail, medium, large, full or custom size)
+                $original = 'full';
+                if ($images): ?>
 
-                <?php foreach ($images as $image_id) :
-                $image_alt = get_post_meta($image_id, '_wp_attachment_image_alt', TRUE); ?>
+                <?php foreach ($images as $image_id):
+                        $image_alt = get_post_meta($image_id, '_wp_attachment_image_alt', TRUE); ?>
                 <div class="mix gallery-image">
                     <a data-fslightbox="gallery" href="<?php echo wp_get_attachment_image_url($image_id, $original); ?>"
                         class="lightbox-gallery" alt="<?php echo $image_alt; ?>">
@@ -52,11 +52,12 @@ if ($images) : ?>
                 </div>
                 <?php endforeach; ?>
                 <?php endif; ?>
-                <?php if( have_rows('videos') ): ?>
-                <?php while( have_rows('videos') ): the_row(); 
-        $image = get_sub_field('poster_image');
-        $file = get_sub_field('video_file');
-        ?><a class="mix gallery-video" data-fslightbox="gallery" href="<?php echo $file['url']; ?>"
+                <?php if (have_rows('videos')): ?>
+                <?php while (have_rows('videos')):
+                        the_row();
+                        $image = get_sub_field('poster_image');
+                        $file = get_sub_field('video_file');
+                        ?><a class="mix gallery-video" data-fslightbox="gallery" href="<?php echo $file['url']; ?>"
                     class="lightbox-gallery" alt="<?php echo $image_alt; ?>">
                     <div class="video-image">
 
@@ -66,15 +67,16 @@ if ($images) : ?>
                 <?php endwhile; ?>
                 <?php endif; ?>
 
-                <?php if( have_rows('web_cams') ): ?>
-                <?php while( have_rows('web_cams') ): the_row(); 
-        $image = get_sub_field('poster_image');
-        $file = get_sub_field('image_url');
-        ?><a class="mix gallery-web" data-fslightbox="gallery-web" data-type="image"
+                <?php if (have_rows('web_cams')): ?>
+                <?php while (have_rows('web_cams')):
+                        the_row();
+                        $image = get_sub_field('poster_image');
+                        $file = get_sub_field('image_url');
+                        ?><a class="mix gallery-web" data-fslightbox="gallery-web" data-type="image"
                     href="<?php the_sub_field('image_url'); ?>" class="lightbox-gallery" alt="<?php echo $image_alt; ?>"
                     data-caption="<h2 class='heading-2'><?php the_sub_field('title'); ?></h2><p><?php the_sub_field('description'); ?></p>">
                     <div class="webcam-image" style="background-image: url('<?php the_sub_field('image_url'); ?>')">
-
+                        <p class="webcam-time"><?php the_sub_field('title'); ?></p>
                     </div>
                 </a>
                 <?php endwhile; ?>
